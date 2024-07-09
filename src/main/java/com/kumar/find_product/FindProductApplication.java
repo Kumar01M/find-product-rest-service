281f130f3f225ec5aa8cd6181ee62faa8096a502
Kumar <kumaranmk33@gmail.com>
2024-07-09 23:19:35 +0530

    src commit: files

---
+++ src/main/java/com/kumar/find_product/FindProductApplication.java
@@ -0,0 +1,13 @@
+package com.kumar.find_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindProductApplication.class, args);
+	}
+
+}
